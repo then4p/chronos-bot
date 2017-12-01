33d75af55dd16a721c5cdb323b39e3a24b5fdd50
Levente Schoenherr <lele5293@gmail.com>
2017-12-01 19:08:21 +0100

    tells the delta for three timezones now added a simple help answer.

--- main.py
+++ main.py
@@ -5,7 +5,14 @@ import timefunctions
 from datetime import datetime
 
 client = discord.Client()
-command = '!time' #this command will call the bot
+
+# ---commands
+command_1 = '!time'  # this command will call the bot
+manual = '!help'  # this command will make the bot explain its functionalities
+
+# ---Options
+debug = True  # set this to 'True' for more detailed debugging output in console
+
 
 @client.event
 async def on_ready():
@@ -14,28 +21,40 @@ async def on_ready():
     print(client.user.id)
     print('------')
 
-    print('logged into Server(s):')
-    for server in client.servers:
-        print(server.name, server.id)
+    if debug:
+        print('logged into Server(s):')
+        for server in client.servers:
+            print(server.name, server.id)
+
 
 @client.event
 async def on_message(message):
-    if command in message.content:
-        counter = 0
-        tmp = await client.send_message(message.channel, 'Warping time...')
+    if command_1 in message.content and message.author.id != client.user.id:
+        bot_message = await client.send_message(message.channel, 'Warping time...')
 
-        # TODO: get the time out of any message, in any format
-        time_position = message.content.index(command) + len(command)       #position of time in the message
-        time = message.content[time_position + 1:len(message.content)]        #hh:mm
+        time_position = message.content.index(command_1) + len(command_1)  # position of time in the message
+        time_string = message.content[time_position + 1:len(message.content)]  # hh:mm
+        time = timefunctions.parser(time_string)  # datetime object with possible time zone info
 
-        print(timefunctions.parse_time(time))
+        if debug: print('Time parsed from message: ' + str(time))
 
-        await client.edit_message(tmp, 'Sick timetable')
+        timetable = ''
+        delta = timefunctions.delta(time)
+        hours, remainder = divmod((timefunctions.delta(time)).seconds, 3600)
+        minutes, seconds = divmod(remainder, 60)
 
-        #TODO main functionility here
+        # TODO: add a table with common timezones
+        #        for tz in timefunctions.TIMEZONES:
+        #            timetable = timetable + '    ' + tz
+
+        await client.edit_message(bot_message,
+                                  'This is in ' + str(hours) + ' hours and '
+                                  + str(minutes) + ' minutes.' + '\n' + timetable)
+
+    if manual in message.content:
+        bot_message = await client.send_message(message.channel,
+                                                'I can give you a delta to your time. \nCall me with \'!time HH:MM TIMEZONE.\'\n'
+                                                'I currently now these time zones: SET (UTC -5), GMT (UTC) and \nCET(UTC + 1))')
+
 
 client.run(secrets.TOKEN)
-
-
-
-
--- timefunctions.py
+++ timefunctions.py
@@ -2,14 +2,26 @@ import datetime
 from dateutil.parser import parse
 from dateutil.tz import *
 
-TZOFFSETS = {"PH":gettz("America/New_York")}
+# add new timezones here
+TIMEZONES = {'SET': gettz('America/New_York'),
+             'GMT': gettz('Europe/London'),
+             'CET': gettz('Europe/Berlin')}
 
-def parse_time(time):
-    return parse(time, tzinfos = TZOFFSETS,  fuzzy = True)
 
-def countdown(time):
-    return time.timedelta(time.gmtime(),time)
+# parses time and ignores the rest of the string, returns a datetime object
+def parser(time):
+    return parse(time, tzinfos=TIMEZONES, fuzzy=True)
 
-def print_time(time):
+
+# converts the timefrom the message into UTC for further manipulation
+def normalize(time):
+    return time
+
+
+# returns a timedelta object
+def delta(time):
+    return time - datetime.datetime.now(TIMEZONES['CET'])
+
+
+def print(time):
     return time.strftime("%H:%M", time)
-
